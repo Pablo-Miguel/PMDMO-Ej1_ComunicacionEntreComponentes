3f531fcc29834b39246e124ef42161dee17fa836
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-26 13:19:30 +0200

    V1

---
+++ src/app/padre/padre.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent implements OnInit {
+
+  texto: String = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  enviarTextoHijo(valorBtn: String) {
+    this.texto = "Has pulsado el btn: " + valorBtn;
+  }
+}
